fcd41c7108e5cca10a47ab865690c82c8acd7923
danushkakasun5 <danushkakasun5@gmail.com>
2023-09-08 12:08:01 +0530

    initial server setup

--- backend/member-service/src/main.ts
+++ backend/member-service/src/main.ts
@@ -1,1 +1,8 @@
-console.log("hello world")
+import express from 'express';
+import env from 'dotenv';
+
+
+env.config();
+const app =express();
+
+app.listen(process.env.SERVER_PORT,()=>console.log(`sever Started using port ${process.env.SERVER_PORT}`))
