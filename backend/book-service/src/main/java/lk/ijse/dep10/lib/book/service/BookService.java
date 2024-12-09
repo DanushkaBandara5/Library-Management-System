d870cf18aa52220285d6fb2321eb41b1cc4e4e8c
danushka bandara <danushkakasun5@gmail.com>
2024-12-09 11:47:18 +0530

    testing commit

--- backend/book-service/src/main/java/lk/ijse/dep10/lib/book/service/BookService.java
+++ backend/book-service/src/main/java/lk/ijse/dep10/lib/book/service/BookService.java
@@ -3,6 +3,7 @@ package lk.ijse.dep10.lib.book.service;
 import lk.ijse.dep10.lib.book.dto.BookDTO;
 
 import java.util.List;
+//this is to test
 
 public interface BookService {
     void save(BookDTO bookDTO);
